105cdf7ca45b20acac2ac1840dcde1b1a489803a
Ranjith Hegde <ranjithshegde@gmail.com>
2025-06-22 13:23:32 +0530

    chore(headers): Remove windows specific API bloat

--- src/config.h
+++ src/config.h
@@ -31,11 +31,8 @@
 #include <complex>
 #include <cstdint>
 
-// Windows-specific includes
+// Cross-platform definitions
 #ifdef MAYAFLUX_PLATFORM_WINDOWS
-#include <shlwapi.h>
-#include <windows.h>
-#pragma comment(lib, "shlwapi.lib")
 
 // Type compatibility
 using u_int8_t = uint8_t;
@@ -43,25 +40,6 @@ using u_int16_t = uint16_t;
 using u_int32_t = uint32_t;
 using u_int64_t = uint64_t;
 
-#ifdef CALLBACK
-#undef CALLBACK
-#endif
-
-#ifdef TRUE
-#undef TRUE
-#endif
-
-#ifdef FALSE
-#undef FALSE
-#endif
-
-#ifdef ERROR
-#undef ERROR
-#endif
-#endif
-
-// Cross-platform definitions
-#ifdef MAYAFLUX_PLATFORM_WINDOWS
 #define MAYAFLUX_EXPORT __declspec(dllexport)
 #define MAYAFLUX_IMPORT __declspec(dllimport)
 #define MAYFALUX_FORCEINLINE __forceinline
