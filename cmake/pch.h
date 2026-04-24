21817c272564bf2e978fe50828d755fe205e02b1
Ranjith Hegde <ranjithshegde@gmail.com>
2026-04-24 14:25:34 +0530

    refactor(pch): Create scoped pch files
    Instead of the current monolithic pch file, we now use a system deps level
    pch_minimal.h and moved config.h dependency to it. The old pch.h now just
    includes pch_minimal.h, adds glm dependency and keeps the rest of the
    template metaprogramming concepts and type traits.
    This allows for targets to not include glm or not be marred by MT/MD issues
    from external dependencies.

--- cmake/pch.h
+++ cmake/pch.h
@@ -1,43 +1,8 @@
 #pragma once
 
-#include "algorithm"
-#include "any"
-#include "array"
-#include "atomic"
-#include "cstdio"
-#include "cstdlib"
-#include "exception"
-#include "filesystem"
-#include "functional"
-#include "iostream"
-#include "list"
-#include "map"
-#include "memory"
-#include "mutex"
-#include "numbers"
-#include "numeric"
-#include "optional"
-#include "ranges"
-#include "set"
-#include "shared_mutex"
-#include "span"
-#include "sstream"
-#include "string"
-#include "thread"
-#include "unordered_map"
-#include "unordered_set"
-#include "utility"
-#include "variant"
-#include "vector"
+#include "pch_minimal.h"
 
 #include <glm/fwd.hpp>
-
-#include <cmath>
-#include <complex>
-#include <cstdint>
-#include <cstring>
-
-#include "config.h"
 
 // === Core MayaFlux Type System ===
 
---
+++ cmake/pch_minimal.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "algorithm"
+#include "any"
+#include "array"
+#include "atomic"
+#include "cstdio"
+#include "cstdlib"
+#include "exception"
+#include "filesystem"
+#include "functional"
+#include "iostream"
+#include "list"
+#include "map"
+#include "memory"
+#include "mutex"
+#include "numbers"
+#include "numeric"
+#include "optional"
+#include "ranges"
+#include "set"
+#include "shared_mutex"
+#include "span"
+#include "sstream"
+#include "string"
+#include "thread"
+#include "unordered_map"
+#include "unordered_set"
+#include "utility"
+#include "variant"
+#include "vector"
+
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstring>
+
+#include "config.h"
